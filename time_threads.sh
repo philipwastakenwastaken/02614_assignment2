3223306b117cf077480851c523374141079d8770
Zhijian Feng <francisya@163.com>
2023-01-11 16:00:31 +0100

    update time_thread

--- time_threads.sh
+++ time_threads.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 EXEC = "./poisson_j"
-SAVE = "./output_j.dat"
+SAVE="./output_j"
 THREADS="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16"
 for T in $THREADS
 do  
     echo "Result for ${T} Threads:"
-    time OMP_NUM_THREADS=${T} ${EXEC} >> ${SAVE}
+    SAVETMP=${SAVE}\_${T}.dat
+    time OMP_NUM_THREADS=${T} 150 5000 0.01 0 ${EXEC} >> ${SAVETMP}
     sleep(3);
 done
